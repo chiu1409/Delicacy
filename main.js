9ee56b854df83de05b529cff14ad58e2a9cf1879
chiu1409 <sadichhatalekar@gmail.com>
2025-02-17 21:53:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper('.swiper-container', {
+    spaceBetween: 30,       // Spacing between slides
+    centeredSlides: true,   // Center the active slide
+    navigation: {
+        nextEl: '.swiper-button-next',  // Next slide button
+        prevEl: '.swiper-button-prev',  // Previous slide button
+    },
+});
